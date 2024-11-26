bcb0d94ae3cf8e6e1ead2f80d61a202186b46c57
Vishal079-code <u24ai079@aid.svnit.ac.in>
2024-11-26 23:38:21 +0530

    Update practical61.c

--- practical61.c
+++ practical61.c
@@ -11,10 +11,10 @@ for(i=0;i<n;i++)
 		printf("enter the number:");
 		scanf("%d",&array[i]);		
 	}
-	for(i=0;i<n;i++)
+	for(i=0;i<n-1;i++)
 	{
 	
-	for(j=0;j<n;j++)
+	for(j=0;j<n-1-i;j++)
 		 {
 			if(array[j]>array[j+1])
 			{
